3503502e45b441939555d1a74e2fbed35f59c105
Aniket <anidok2008@gmail.com>
2019-10-08 13:46:28 +0530

    Add Cryptography Helper for password decryption.

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -3,3 +3,4 @@ from .mongo_connection import *
 from .news_app import *
 from .news_article import *
 from .datetime_provider import *
+from .cryptography_helper import *
---
+++ tests/test_cryptography_helper.py
@@ -0,0 +1,20 @@
+import unittest
+from mockito import mock, unstub, verify, ANY, when
+from src.scraper.cryptography_helper import CryptographyHelper
+
+
+class TestCryptographyHelper(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_no_password(self):
+        self.assertIsNone(CryptographyHelper.decrypt(None))
+
+    def test_encrypted_pasword(self):
+        encrypted_password = 'gAAAAABdnEVcJYKGTxRsDG3mP0QFK9TH3oWMhlOZpdvDX_d5PEdXwrZah5XFsWgr8BV3uACb6oaleXdARvca_G7NtovOZ-4zvg=='
+        decrypted_password = CryptographyHelper.decrypt(encrypted_password)
+        self.assertEqual('justpassword', decrypted_password)
+
+    def tearDown(self):
+        unstub()
